035100a4dfc14e585a5c2da399b2380a76cbf4f3
csanbar97 <cristianbs97@gmail.com>
2023-05-18 11:03:28 +0200

    procedimiento almacenado de logs

---
+++ database/migrations/2023_05_18_085217_logs.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('logs', function (Blueprint $table) {
+      $table->increments('id');
+      $table->date('fecha');
+      $table->time('hora');
+      $table->unsignedBigInteger('usuario');
+      $table->timestamps();
+
+      $table->foreign('usuario')->references('id')->on('users');
+    });
+
+    $procedimiento = "
+        CREATE PROCEDURE `insertarLog`(`fecha` DATE, `hora` TIME, `operacion` VARCHAR(20), `usuario` VARCHAR(20))
+        BEGIN
+            INSERT INTO logs (fecha, hora, operacion, usuario) VALUES (fecha, hora, operacion, usuario);
+        END
+        ";
+
+    DB::unprepared($procedimiento);
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    //
+  }
+};
