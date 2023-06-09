9409bc07da38c3ad6261c9439a3ea76688055c55
csanbardev <csanbar97@gmail.com>
2023-06-09 13:28:26 +0200

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -15,7 +15,7 @@ Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register')->middleware('can:admin');
 
-    Route::post('register', [RegisteredUserController::class, 'store'])->middleware('can:admin');
+    Route::post('register', [RegisteredUserController::class, 'store'])
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
